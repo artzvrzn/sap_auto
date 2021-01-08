a66486af18f6fd694b3fbec2c5ab4e10acf5fbc2
BY059491 <jojoba37544>
2021-01-08 23:45:28 +0300

    transaction names dict with images

---
+++ transaction_names.py
@@ -0,0 +1,12 @@
+region = (1525, 1010, 1725, 1055)
+
+names = {'sap_easy_access': 'sap_transactions\\sap_easy_access.png',
+         'zihp': 'sap_transactions\\zihp.png',
+         'zihp_change_first': 'sap_transactions\\zihp_change_first.png',
+         'zihp_output': 'sap_transactions\\zihp_output.png',
+         'shipment_messages_first': 'sap_transactions\\shipment_messages_first.png',
+         'shipment_messages_sec': 'sap_transactions\\shipment_messages_sec.png',
+         'zlpc': 'sap_transactions\\zlpc.png',
+         'zlpc_details': 'sap_transactions\\zlpc_details.png',
+         'zlpc_replenish': 'sap_transactions\\zlpc_replenish.png',
+        }
