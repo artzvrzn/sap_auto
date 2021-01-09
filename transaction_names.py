7155af0f4fa5944fc4c04a81c93439eb2fde0df6
BY059491 <jojoba37544>
2021-01-09 03:28:53 +0300

    working on transaction starting itself whatever status it has

--- transaction_names.py
+++ transaction_names.py
@@ -1,12 +1,24 @@
 names_region = (1525, 1010, 1725, 1055)
 
 transaction_names = {'sap_easy_access': 'sap_transactions\\sap_easy_access.png',
-         'zihp': 'sap_transactions\\zihp.png',
-         'zihp_change_first': 'sap_transactions\\zihp_change_first.png',
-         'zihp_output': 'sap_transactions\\zihp_output.png',
-         'shipment_messages_first': 'sap_transactions\\shipment_messages_first.png',
-         'shipment_messages_sec': 'sap_transactions\\shipment_messages_sec.png',
-         'zlpc': 'sap_transactions\\zlpc.png',
-         'zlpc_details': 'sap_transactions\\zlpc_details.png',
-         'zlpc_replenish': 'sap_transactions\\zlpc_replenish.png',
-        }
+                     'zihp': 'sap_transactions\\zihp.png',
+                     'zihp_data': 'sap_transactions\\zihp_data.png',
+                     'zihp_change_first': 'sap_transactions\\zihp_change_first.png',
+                     'zihp_output': 'sap_transactions\\zihp_output.png',
+                     'shipment_messages_first': 'sap_transactions\\shipment_messages_first.png',
+                     'shipment_messages_sec': 'sap_transactions\\shipment_messages_sec.png',
+                     'zlpc': 'sap_transactions\\zlpc.png',
+                     'zlpc_details': 'sap_transactions\\zlpc_details.png',
+                     'zlpc_replenish': 'sap_transactions\\zlpc_replenish.png',
+                     'new_mode': 'sap_transactions\\new_mode.png',
+                     }
+
+titles_list = ['ZLPC',
+               'In-house processing 2300',
+               'Stock transfer: Warehouse 270',
+               'Replenish picking area in warehouse 270 to maximum quantity',
+               'In-house processing',
+               'Logistic Documents Print Program',
+               'Replenish picking area in warehouse 270 to maximum quantity P03 (1)',
+               'Load processing cockpit: Warehouse 270 - Minsk PRD P03',
+               ]
--- windows.py
+++ windows.py
@@ -1,6 +1,6 @@
 import pygetwindow as gw
 import pyautogui as gui
-from transaction_names import transaction_names, names_region
+from transaction_names import transaction_names, names_region, titles_list
 from time import time
 
 # print(locals().get('transaction_name').title)
@@ -9,31 +9,68 @@ print(gw.getAllTitles())
 
 # zlpc = gw.getWindowsWithTitle('ZLPC')[0]
 # zihp = gw.getWindowsWithTitle('In-house processing 2300')[0]
-sap_main = gw.getWindowsWithTitle('SAP Easy Access')[0]
+# sap_main = gw.getWindowsWithTitle('SAP Easy Access')[0]
 # zlt10 = gw.getWindowsWithTitle('Stock transfer: Warehouse 270')[0]
 
-#
-# def zlpc():
-#     try:
-#         title = gw.getWindowsWithTitle('ZLPC')[0]
-#     except IndexError:
-#         pass
+
+class Transaction:
+
+    def sap_access(self):
+        try:
+            sap_main = gw.getWindowsWithTitle('SAP Easy Access')[0]
+            sap_main.activate()
+        except IndexError:
+            print('Seems there is no opened main SAP window')
+            for transaction in titles_list:
+                if transaction == 'ZLPC':
+                    continue
+                elif transaction in gw.getAllTitles():
+                    temp_transaction = gw.getWindowsWithTitle(f'{transaction}')[0]
+                    temp_transaction.activate()
+                    while True:
+                        gui.locateOnScreen(transaction_names['new_mode'], region=(410, 30, 575, 60))
+                        gui.hotkey('ctrl', 'n')
+                        gui.locateOnScreen(transaction_names['sap_easy_access'], region=names_region)
+                        break
+
+    def zihp(self):
+        self.sap_access()
+        while True:
+            if gui.locateOnScreen(transaction_names['sap_easy_access'], region=names_region):
+                gui.click(x=100, y=50, duration=0)
+                gui.hotkey('ctrl', 'left')
+                gui.hotkey('ctrl', 'delete')
+                gui.write('ZIHP')
+                gui.press('enter')
+                break
+        while True:
+            if gui.locateOnScreen(transaction_names['zihp_data'], region=(0, 120, 320, 200)):
+                gui.hotkey('ctrl', 'a')
+                gui.press('del')
+                gui.write('2300')
+                gui.press('down')
+                gui.write('2310')
+                gui.press('down', presses=2)
+                gui.write('1')
+                gui.press('tab')
+                gui.write('5')
+                gui.hotkey('shift', 'tab')
+                gui.press('down', presses=4)
+                gui.hotkey('ctrl', 'a')
+                gui.press('space')
+                gui.press('f8')
+                break
 
 
-def open_transaction():
-    sap_main.activate()
-    while True:
-        if gui.locateOnScreen(transaction_names['sap_easy_access'], region=names_region):
-            gui.click(x=100, y=50, duration=0)
-            gui.hotkey('ctrl', 'right')
-            gui.hotkey('ctrl', 'left')
-            gui.hotkey('ctrl', 'delete')
-            gui.write('ZIHP')
-            gui.press('enter')
-            break
+# def open_transaction(transaction_name):
+#     sap_main.activate()
+#     if transaction_name == 'zihp':
 
-open_transaction()
 
+# Transaction().zihp()
+while True:
+    gui.sleep(1)
+    print(gw.getActiveWindow())
 
 
 
